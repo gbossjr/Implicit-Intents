9b44f94bc7a283376ccf15335190a0e934ea2b15
Geoffrey Bosire Jr <gbosirejr@gmail.com>
2020-05-21 17:48:18 -0800

    first commit

---
+++ app/src/main/java/android/example/implicitintents/MainActivity.java
@@ -0,0 +1,29 @@
+package android.example.implicitintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.drm.DrmStore;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mWebsiteEditText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebsiteEditText = findViewById(R.id.website_editText);
+    }
+
+    public void openWebsite(View view) {
+        String url = mWebsiteEditText.getText().toString();
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+    }
+}
